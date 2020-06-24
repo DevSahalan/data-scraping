5ad86d47ad793e5ee97454f024faa41f3db6c375
sahalan hasan <mythsahalan@yahoo.com>
2020-06-24 23:11:00 +0600

    first web scraper

---
+++ First-Web-Scraper/index.js
@@ -0,0 +1,24 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+async function main() {
+  const html = await request.get("http://127.0.0.1:5500/index.html");
+  fs.writeFileSync("./text.html", html);
+  const $ = await cheerio.load(html);
+  const theText = $("h1").text();
+  const theLinkText = $("a").text();
+  $("h2").each((index, element) => {
+    console.log($(element).text());
+  });
+  console.log(theText);
+  console.log(theLinkText);
+  const redId = $("#red").text();
+  console.log(redId);
+  const redClass = $(".red").text();
+  console.log(redClass);
+  const attribute = $('[data-customer="22025"]').text();
+  console.log(attribute);
+}
+
+main();

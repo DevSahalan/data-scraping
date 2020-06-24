3bdf6c01c49d416efe9f6fec37d0a54dd6a06a80
sahalan hasan <mythsahalan@yahoo.com>
2020-06-24 23:14:53 +0600

    async try catch added

--- First-Web-Scraper/index.js
+++ First-Web-Scraper/index.js
@@ -3,22 +3,27 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 async function main() {
-  const html = await request.get("http://127.0.0.1:5500/index.html");
-  fs.writeFileSync("./text.html", html);
-  const $ = await cheerio.load(html);
-  const theText = $("h1").text();
-  const theLinkText = $("a").text();
-  $("h2").each((index, element) => {
-    console.log($(element).text());
-  });
-  console.log(theText);
-  console.log(theLinkText);
-  const redId = $("#red").text();
-  console.log(redId);
-  const redClass = $(".red").text();
-  console.log(redClass);
-  const attribute = $('[data-customer="22025"]').text();
-  console.log(attribute);
+  try {
+    const html = await request.get("http://127.0.0.1:5500/index.html");
+    fs.writeFileSync("./text.html", html);
+    const $ = await cheerio.load(html);
+    const theText = $("h1").text();
+    const theLinkText = $("a").text();
+
+    console.log(theText);
+    console.log(theLinkText);
+    const redId = $("#red").text();
+    console.log(redId);
+    const redClass = $(".red").text();
+    console.log(redClass);
+    const attribute = $('[data-customer="22025"]').text();
+    console.log(attribute);
+    $("h2").each((index, element) => {
+      console.log($(element).text());
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 main();
